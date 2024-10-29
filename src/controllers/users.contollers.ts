4c2d2c01592d97e49d3b917cf85470bfed202286
Hminhman2908 <huynhman2908@gmail.com>
2024-10-29 21:47:57 +0700

    login register

--- src/controllers/users.contollers.ts
+++ src/controllers/users.contollers.ts
@@ -1,15 +1,53 @@
 import { Request, Response } from 'express'
+import userService from '~/services/users.services'
 
-export const loginController = (req: Request, res: Response) => {
-  const { username, password } = req.body
-  if (username === 'huynhman2908@gmail.com' && password === '123456') {
-    res.json({
-      message: 'Logged in successfully!'
+export const loginController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  /*--------------------------------------------------------------*/
+  // if (email === 'huynhman2908@gmail.com' && password === '123456') {
+  //   return res.json({
+  //     message: 'Logged in successfully!'
+  //   })
+  // }
+
+  // res.status(401).json({
+  //   message: 'Login failed! Incorrect username or password.'
+  // })
+  /*--------------------------------------------------------------*/
+  try {
+    const result = await userService.login({ email, password })
+    return res.json({
+      message: 'Logged in successfully!',
+      result: {
+        userId: result._id,
+        email,
+        password
+      }
     })
-    return
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Login failed! Incorrect email or password.',
+      error
+    })
   }
+}
 
-  res.status(401).json({
-    message: 'Login failed! Incorrect username or password.'
-  })
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = userService.register({ email, password })
+    return res.json({
+      message: 'Registration successful!',
+      result: {
+        userId: (await result).insertedId,
+        email,
+        password
+      }
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Register failed!',
+      error
+    })
+  }
 }
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,5 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Db, Collection } from 'mongodb'
 import { config } from 'dotenv'
+import User from '~/models/schemas/Users.schema'
 
 // Load environment variables from.env file
 config()
@@ -8,18 +9,24 @@ const uri = `mongodb+srv://${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PAS
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
   async connect() {
     try {
       // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+    } catch (error) {
+      console.log(error)
+      throw error
     }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
   }
 }
 
