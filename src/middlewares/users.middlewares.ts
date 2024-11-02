afe7f2ea5f98ea802425e59892c580d63ca6dab8
Hminhman2908 <huynhman2908@gmail.com>
2024-11-02 20:21:55 +0700

    mesage validation

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
+import { USERS_MESSAGES } from '~/constants/messages'
 import { ErrorWithStatus } from '~/models/Errors'
 import userService from '~/services/users.services'
 import { validate } from '~/ultis/validation'
@@ -15,39 +16,63 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
 export const registerValidator = validate(
   checkSchema({
     name: {
-      isLength: { options: { min: 1, max: 100 } },
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.NAME_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: USERS_MESSAGES.NAME_MUST_BE_A_STRING
+      },
+      isLength: { options: { min: 1, max: 100 }, errorMessage: USERS_MESSAGES.NAME_LENGTH_MUST_BE_FROM_1_TO_100 },
       trim: true,
       errorMessage: 'Invalid name'
     },
     email: {
-      notEmpty: true,
-      isEmail: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.EMAIL_IS_REQUIRED
+      },
+      isEmail: {
+        errorMessage: USERS_MESSAGES.EMAIL_IS_INVALID
+      },
       trim: true,
       custom: {
         options: async (value) => {
           const isExistEmail = await userService.checkEmailExistence(value)
           if (isExistEmail) {
-            throw new Error('Email already exists')
+            throw new Error(USERS_MESSAGES.EMAIL_ALREADY_EXISTS)
           }
           return true
         }
       }
     },
     password: {
-      notEmpty: true,
-      isLength: { options: { min: 8, max: 100 } },
+      notEmpty: { errorMessage: USERS_MESSAGES.PASSWORD_IS_REQUIRED },
+      isString: { errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_A_STRING },
+      isLength: { options: { min: 6, max: 50 }, errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50 },
       isStrongPassword: {
-        options: { minLength: 8, minLowercase: 1, minNumbers: 1, minSymbols: 1, minUppercase: 1 }
+        options: { minLength: 6, minLowercase: 1, minNumbers: 1, minSymbols: 1, minUppercase: 1 }
       },
-      errorMessage: 'Invalid password'
+      errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
     },
     confirm_password: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_IS_REQUIRED
+      },
+      isString: {
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_A_STRING
+      },
+      isLength: {
+        options: { min: 6, max: 50 },
+        errorMessage: USERS_MESSAGES.PASSWORD_LENGTH_MUST_BE_FROM_6_TO_50
+      },
+      isStrongPassword: {
+        options: { minLength: 6, minLowercase: 1, minNumbers: 1, minSymbols: 1, minUppercase: 1 },
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
+      },
+      errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG,
       custom: {
         options: (value, { req }) => {
           if (value !== req.body.password) {
-            throw new Error('Confirm password does not match')
+            throw new Error(USERS_MESSAGES.CONFIRM_PASSWORD_MUST_BE_THE_SAME_AS_PASSWORD)
           }
           return true
         }
@@ -55,7 +80,7 @@ export const registerValidator = validate(
     },
     date_of_birth: {
       isISO8601: { options: { strict: true, strictSeparator: true } },
-      errorMessage: 'Invalid date of birth'
+      errorMessage: USERS_MESSAGES.DATE_OF_BIRTH_MUST_BE_ISO8601
     }
   })
 )
